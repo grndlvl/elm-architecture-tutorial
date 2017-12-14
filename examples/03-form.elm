31b067dabf967a2f814b75378b207f27e04cf03b
Jonathan DeLaigle <jonathan@gollygoodsoftware.com>
2017-12-14 12:51:58 -0500

    Inital commit

--- examples/03-form.elm
+++ examples/03-form.elm
@@ -1,6 +1,10 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
+import String exposing (..)
+import Char exposing (..)
+import Result exposing (withDefault)
+import Debug exposing (..)
 
 
 main =
@@ -19,12 +23,15 @@ type alias Model =
   { name : String
   , password : String
   , passwordAgain : String
+  , age : String
+  , error : String
+  , color : String
   }
 
 
 model : Model
 model =
-  Model "" "" ""
+  Model "" "" "" "" "" ""
 
 
 
@@ -35,6 +42,8 @@ type Msg
     = Name String
     | Password String
     | PasswordAgain String
+    | Age String
+    | Validate
 
 
 update : Msg -> Model -> Model
@@ -49,7 +58,14 @@ update msg model =
     PasswordAgain password ->
       { model | passwordAgain = password }
 
+    Age age ->
+      { model | age = age }
 
+    Validate ->
+      let
+          (color, error) = validate model
+      in
+        { model | color = color, error = error }
 
 -- VIEW
 
@@ -60,17 +76,37 @@ view model =
     [ input [ type_ "text", placeholder "Name", onInput Name ] []
     , input [ type_ "password", placeholder "Password", onInput Password ] []
     , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
-    , viewValidation model
+    , input [ type_ "text", placeholder "Age", onInput Age ] []
+    , div [ style [("color", model.color)] ] [ text model.error ]
+    , button [ onClick Validate ] [ text "Submit" ]
     ]
 
+validate : Model -> (String, String)
+validate model =
+  if length model.age > 0 && not (isNumber model.age) then
+    ("red", "Age must be a valid number.")
+  else if length model.password > 0 then
+    if length model.password < 8 then
+      ("red", "Password must be greater than 8 characters!")
+    else if not (any isDigit model.password) then
+      ("red", "Password must contain a numeric caracter.")
+    else if not (any isLower model.password) then
+      ("red", "Password must contain a lowercase character.")
+    else if not (any isUpper model.password) then
+      ("red", "Password must contain a uppercase character.")
+    else if not (model.password == model.passwordAgain) then
+      ("red", "Passwords do not match!")
+    else
+      ("green", "OK")
+  else
+    ("green", "OK")
 
-viewValidation : Model -> Html msg
-viewValidation model =
+
+isNumber : String -> Bool
+isNumber s =
   let
-    (color, message) =
-      if model.password == model.passwordAgain then
-        ("green", "OK")
-      else
-        ("red", "Passwords do not match!")
+      n = toInt s
   in
-    div [ style [("color", color)] ] [ text message ]
+    case n of
+      (Ok num) -> True
+      _ -> False
--- examples/04-random.elm
+++ examples/04-random.elm
@@ -1,4 +1,5 @@
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Random
 
@@ -18,13 +19,14 @@ main =
 
 
 type alias Model =
-  { dieFace : Int
+  { die1Face : Int
+  , die2Face : Int
   }
 
 
 init : (Model, Cmd Msg)
 init =
-  (Model 1, Cmd.none)
+  (Model 1 1, Cmd.none)
 
 
 
@@ -33,17 +35,17 @@ init =
 
 type Msg
   = Roll
-  | NewFace Int
+  | NewFace Int Int
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Roll ->
-      (model, Random.generate NewFace (Random.int 1 6))
+      (model, Random.generate NewFace (Random.pair (Random.int 1 6) (Random.int 1 6)) )
 
-    NewFace newFace ->
-      (Model newFace, Cmd.none)
+    NewFace new1Face new2Face ->
+      (Model new1Face new2Face, Cmd.none)
 
 
 
@@ -61,7 +63,27 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ h1 [] [ text (toString model.dieFace) ]
-    , button [ onClick Roll ] [ text "Roll" ]
-    ]
+  let
+      (x1Offset, y1Offset) = getOffset model.die1Face
+  in
+    div []
+      [ h1 [] [ text (toString model.die1Face) ]
+      , div [ style [("background", "url(./dice.png) " ++ x1Offset ++ " " ++ y1Offset), ("width", "112px"), ("height", "122px")] ] []
+      --, div [ style [("background", "url(./dice.png) " ++ xOffset ++ " " ++ yOffset), ("width", "112px"), ("height", "122px")] ] []
+      , button [ onClick Roll ] [ text "Roll" ]
+      ]
+
+getOffset : Int -> (String, String)
+getOffset x =
+  if x == 2 then
+    ("-114.5px", "0")
+  else if x == 3 then
+    ("-224px", "0")
+  else if x == 4 then
+    ("0", "-122px")
+  else if x == 5 then
+    ("-115px", "-122px")
+  else if x == 6 then
+    ("-224px", "-122px")
+  else
+    ("0", "0")
